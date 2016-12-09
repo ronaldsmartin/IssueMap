ed6dff66e68e8cba1ed0a9f68d74a6c1319e92b4
Ronald Martin <ronald@ronaldsmartin.com>
2016-12-09 11:57:20 -0800

    Bind Bitbucket API results to bottom sheet tableview.

--- Issue Map/Controllers/IssuesListViewController.swift
+++ Issue Map/Controllers/IssuesListViewController.swift
@@ -7,18 +7,39 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Moya_ObjectMapper
 
 class IssuesListViewController: UIViewController {
+    
+    @IBOutlet private weak var tableView: UITableView!
+    
+    private let reuseID = "Issue Cell"
+    private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        bindTableData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func bindTableData() {
+        bitbucketAPI.request(.issues)
+            .mapObject(IssueAPIResult.self)
+            .map { $0.issues }
+            .bindTo(tableView.rx.items(cellIdentifier: reuseID)) {
+                row, item, cell in
+                cell.textLabel?.text = item.title
+                cell.detailTextLabel?.text = item.content
+        }
+        .addDisposableTo(disposeBag)
     }
     
 
