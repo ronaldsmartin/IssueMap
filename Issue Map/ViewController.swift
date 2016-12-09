8fdda915f567098bd15ec6c0e8d634a673667853
Ronald Martin <ronald@ronaldsmartin.com>
2016-12-09 11:33:15 -0800

    Added bottom sheet with gesture scrolling.

--- Issue Map/ViewController.swift
+++ Issue Map/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var bottomSheet: UIView!
+    
+    @IBOutlet private weak var bottomSheetTopConstraint: NSLayoutConstraint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        bottomSheet.layer.cornerRadius = 8
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onSheetGesture(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: bottomSheet)
+        let duration = TimeInterval(translation.y / sender.velocity(in: bottomSheet).y)
+        
+        UIView.animate(
+            withDuration: duration,
+            animations: {
+                self.bottomSheetTopConstraint.constant += translation.y
+                self.bottomSheet?.setNeedsLayout()
+        },
+            completion: { finished in
+                sender.setTranslation(CGPoint.zero, in: self.bottomSheet)
+        })
+        
+    }
 
 }
 
